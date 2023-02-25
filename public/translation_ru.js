c37161702c0d603048ecc4c55522dbaa10cef300
AnnLaym <rekohoo@gmail.com>
2023-02-25 20:10:36 +0200

    disaen 2

--- public/translation_ru.js
+++ public/translation_ru.js
@@ -4,10 +4,10 @@ window.t = (eng) => {
         'Enter': 'Войти',
         'Not enough players (minimum 3)': 'Слишком мало игроков (минимум 4)',
         'Host can start game': 'Хост может начать игру',
-        'Wait for players to write their hints': 'Подождите, пока игроки напишут мины',
-        'Write your hint': 'Напишите мину!',
+        'Wait for players to write their hints': 'Подождите, пока игроки напишут «слова-мины»',
+        'Write your hint': 'Напишите «слово-мину»!',
         'Wait for players to delete duplicates': 'Загадайте слово не наступая на мины',
-        'Delete duplicates': 'Следите, что б никто не наступил на мины',
+        'Delete duplicates': 'Следите, чтобы никто не наступил на мину',
         'Now try guess the original word': 'Теперь попробуйте угадать исходное слово',
         'Now ': 'Теперь ',
         ' should guess original word': ' должен угадать исходное слово',
@@ -23,6 +23,7 @@ window.t = (eng) => {
         'goal': 'Очки для победы',
         'empty': 'Пусто'
     };
+
     if (translations.hasOwnProperty(eng)) {
         return translations[eng];
     } else {
